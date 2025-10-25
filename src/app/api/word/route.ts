054106f802da17e9c61e279909e62e792150aba1
Supanida111 <Supanida.mekw@bumail.net>
2025-10-25 11:12:34 +0700

    sub1

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -4,5 +4,6 @@ import { words } from "@/data/words";
 export async function GET() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const word = words[randomIndex];
+  console.log('Tangmo...');
   return NextResponse.json({ word });
 }
